20c602fc5668519b0d2d53cd48a8cb8aeb7d54d5
ianreve <ianrevecacao@gmail.com>
2022-02-17 23:54:37 +0000

    Day 1

---
+++ Day 1/day1Exercise.py
@@ -0,0 +1,38 @@
+# 1.Check the python version you are using
+"python3 --version"
+
+# 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+
+add = 1 + 2
+substractions = 1 - 2
+multiplication = 1*2
+modulus = 1%2
+division = 1/2
+exponential = 1**2
+floorDivision = 1//2
+
+# Write strings on the python interactive shell. The strings are the following:
+
+firstName = 'Ian'
+lastName = 'Cacao'
+country = 'UK'
+sentence = 'I am enjoying 30 days of Python'
+
+# 4. Check the data types of the following data:
+type(10)
+type(9.8)
+type(3.14)
+type(4-4j)
+type(['Asabeneh', 'Python', 'Finland'])
+type(firstName)
+type(lastName)
+type(country)
+
+# ===========================================
+
+# Exercise: Level 2 
+# """Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4. Remember to use print() when you are working on a python file. Navigate to the directory where you have saved your file, and run it."""
+print(f"add : {add} , difference : {substractions}, priduct: {multiplication}")
+print(firstName, lastName, country, sentence,)
+
+
---
+++ Day 1/helloworld.py
@@ -0,0 +1,50 @@
+# this is a the first comment made at 17/02/2022 by Ian Reve
+"""This is a 
+multiline comment"""
+
+"Data Types"
+# integer 
+# float
+# complex Example
+"""1 + j, 2 + 4j"""
+
+"Boolean"
+True 
+False
+
+"List" # can be same of diffent types
+[0, 1, 2, 3, 4, 5]  # all are the same data types - a list of numbers
+['Banana', 'Orange', 'Mango', 'Avocado'] # all the same data types - a list of strings (fruits)
+['Finland','Estonia', 'Sweden','Norway'] # all the same data types - a list of strings (countries)
+['Banana', 10, False, 9.81] # different data types in the list - string, integer, boolean and float
+
+
+"Dictionary" 'unordered' # A Python dictionary object is an unordered collection of data in a key value pair format.
+
+{
+    'firstName': 'Ian Reve',
+    'Skills': ['Python', 'JS', 'HTML','CSS' ]
+
+
+}
+
+"Tuple" 'Ordered List' # A tuple is an ordered collection of different data types like list but tuples can not be modified once they are created. They are immutable.
+
+('Asabeneh', 'Pawel', 'Brook', 'Abraham', 'Lidiya') # Names
+('Earth', 'Jupiter', 'Neptune', 'Mars', 'Venus', 'Saturn', 'Uranus', 'Mercury') # planets
+
+"SET" # A set is a collection of data types similar to list and tuple. Unlike list and tuple, set is not an ordered collection of items. Like in Mathematics, set in Python stores only unique items.
+
+{2, 4, 3, 5}
+{3.14, 9.81, 2.7} # order is not important in set
+
+
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
